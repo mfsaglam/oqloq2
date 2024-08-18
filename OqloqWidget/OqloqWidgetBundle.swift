b2b1f507cb5f2aaa6db155676d8e542db05dd127
Fatih Saglam <m.saglam@enbw.com>
2024-08-18 12:55:51 +0300

    add oqloq widget

---
+++ OqloqWidget/OqloqWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  OqloqWidgetBundle.swift
+//  OqloqWidget
+//
+//  Created by Saglam, Fatih on 18.08.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OqloqWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        OqloqWidget()
+        OqloqWidgetLiveActivity()
+    }
+}
