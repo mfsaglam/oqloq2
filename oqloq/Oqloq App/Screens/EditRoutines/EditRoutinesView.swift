e358deefb235467a5f55ac7d7bd55929b739c325
mfsaglam <mfsaglam1@gmail.com>
2024-08-31 16:46:38 +0300

    implement removeAll method

--- oqloq/Oqloq App/Screens/EditRoutines/EditRoutinesView.swift
+++ oqloq/Oqloq App/Screens/EditRoutines/EditRoutinesView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct EditRoutinesView: View {
+    @Environment(\.presentationMode) var presentationMode
     @ObservedObject var vm = EditRoutinesViewModel(interactor: RealmPersistenceInteractor())
+    @State private var showAlert = false
+
     var body: some View {
         List {
             ForEach(vm.routines) { routine in
@@ -23,11 +26,36 @@ struct EditRoutinesView: View {
         .onAppear {
             vm.loadRoutines()
         }
+        .toolbar {
+            ToolbarItem {
+                Button {
+                    showAlert = true
+                } label: {
+                    Text("Remove All")
+                        .foregroundStyle(.red)
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("Confirm Removal"),
+                        message: Text("Are you sure you want to remove all routines? This action cannot be undone."),
+                        primaryButton: .destructive(Text("Remove All")) {
+                            removeAllRoutines()
+                        },
+                        secondaryButton: .cancel()
+                    )
+                }
+            }
+        }
     }
     
     func deleteRoutine(at offsets: IndexSet) {
         // show alert to make sure
          vm.delete(atOffsets: offsets)
+    }
+    
+    func removeAllRoutines() {
+        vm.deleteAllRoutines()
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
--- oqloq/Oqloq App/Screens/EditRoutines/EditRoutinesViewModel.swift
+++ oqloq/Oqloq App/Screens/EditRoutines/EditRoutinesViewModel.swift
@@ -28,13 +28,27 @@ class EditRoutinesViewModel: ObservableObject {
     func delete(atOffsets indexSet: IndexSet) {
         do {
             try indexSet.forEach { index in
-                let routine = routines[index]
-                try interactor.deleteRoutine(routine)
-                NotificationCenter.shared.cancelScheduledNotification(with: routine.id.uuidString)
+                try deleteRoutine(at: index)
             }
             routines.remove(atOffsets: indexSet)
         } catch {
             print(error)
         }
+    }
+    
+    func deleteAllRoutines() {
+        do {
+            try routines.indices.forEach { index in
+                try deleteRoutine(at: index)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func deleteRoutine(at index: IndexSet.Element) throws {
+        let routine = routines[index]
+        try interactor.deleteRoutine(routine)
+        NotificationCenter.shared.cancelScheduledNotification(with: routine.id.uuidString)
     }
 }
